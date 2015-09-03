6e6e8ee166f91637ab57375d6ef5bc635655e99b
rajat404 <404rajat@gmail.com>
2015-09-03 08:54:15 +0530

    added new file for handling all github operations

---
+++ get-github-repo.py
@@ -0,0 +1,44 @@
+import sys
+import os
+from getpass import getpass
+from github3 import GitHub
+
+
+def main():
+    username = raw_input("Enter username: ")
+    password = getpass()
+    githubObj = GitHub(username, password)
+
+    ssh_url_list = []
+    html_url_list = []
+
+    choice = raw_input("Do you want to clone the repositories of your organization? [Y/N] ")
+    if choice == 'y' or choice == 'Y':
+        all_repos = githubObj.repositories()
+        for repo in all_repos:
+            ssh_url_list.append(repo.ssh_url)
+            html_url_list.append(repo.html_url)
+    else:
+        user_repos = githubObj.repositories_by(username)
+        for repo in user_repos:
+            ssh_url_list.append(repo.ssh_url)
+            html_url_list.append(repo.html_url)
+
+    clone_choice = raw_input("Do you want to clone via SSH [Y/N] ")
+    if choice == 'y' or choice == 'Y':
+        print "Initiating Cloning via SSH"
+        for url in ssh_url_list:
+            os.system("git clone " + url)
+
+    else:
+        print "Initiating Cloning via HTTPS"
+        for url in html_url_list:
+            os.system("git clone " + url)
+
+
+if __name__ == '__main__':
+    try:
+        main()
+    except:
+        print "\nQuiting..."
+        sys.exit(0)
